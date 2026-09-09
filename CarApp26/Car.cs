d940cb9c567538333e974f63a7e83501c007122c
Poul Henriksen <pohe@zealand.dk>
2026-09-09 13:03:28 +0200

    Add project files.

---
+++ CarApp26/Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarApp26
+{
+    public class Car
+    {
+        //Instance fields
+        private string _brand;
+        private string _model;
+        private int _year;
+        private string _regNo;
+        private bool _trackReady;
+
+        public Car( string brand, string model, int year, string regNo, bool trackReady )
+        {
+            _brand = brand;
+            _model = model;
+            _year = year;
+            _regNo = regNo;
+            _trackReady = trackReady;
+        }
+
+
+        public Car()
+        {
+            _brand = "Brand";
+            _model = "Model";
+            _year = 1970;
+            _regNo = "xxxxxx";
+            _trackReady = false; 
+        }
+
+
+        public string Brand
+        {
+            get { return _brand; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+        }
+
+        public int  Year
+        {
+            get { return _year; }
+        }
+
+        public string RegNo
+        {
+            get { return _regNo; }
+            set { _regNo = value;  }
+        }
+
+        public bool TrackReady
+        {
+            get { return _trackReady; }
+            set { _trackReady = value;  }
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"Brand {_brand} regno {_regNo}  kører");
+        }
+
+
+        public void Start()
+        {
+            Console.WriteLine("Bilen er startet");
+        }
+
+        public void PrintAllINformation()
+        {
+            Console.WriteLine($"Brand {_brand} model {_model} year {_year} regNo {_regNo} trackReady {_trackReady}");
+        }
+    }
+}
---
+++ CarApp26/Program.cs
@@ -0,0 +1,29 @@
+﻿using CarApp26;
+
+Console.WriteLine("Hello, World!");
+
+
+Car myCar1 = new Car("Skoda", "Octavia", 2020, "AB 232323", true);
+Console.WriteLine($"  {myCar1.Brand}  {myCar1.Model} {myCar1.Year} {myCar1.RegNo} {myCar1.TrackReady}");
+
+myCar1.RegNo = "ZZ343434";
+Console.WriteLine($"  {myCar1.Brand}  {myCar1.Model} {myCar1.Year} {myCar1.RegNo} {myCar1.TrackReady}");
+Car myCar2 = new Car("Lada", "Riva", 2010, "DD 266712", false);
+Console.WriteLine($"  {myCar2.Brand}  {myCar2.Model} {myCar2.Year} {myCar2.RegNo} {myCar2.TrackReady}");
+myCar2.TrackReady = true;
+
+myCar2.Drive();
+
+Car myCar3 = new Car();
+Console.WriteLine($"  {myCar3.Brand}  {myCar3.Model} {myCar3.Year} {myCar3.RegNo} {myCar3.TrackReady}");
+myCar3.TrackReady = true;
+
+myCar1 = myCar3;
+myCar3.RegNo = "AB123123";
+
+
+Console.WriteLine("Her udskriv al information om bilerne");
+
+myCar1.PrintAllINformation();
+myCar2.PrintAllINformation();
+myCar3.PrintAllINformation();
